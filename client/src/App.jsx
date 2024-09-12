ef3a1c19121b6503ba2e27777859910f71423ee4
skricode <skricode@gmail.com>
2024-09-12 18:27:07 +0530

    env config data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import { Button } from "@chakra-ui/react"
+import { Box, Button, useColorModeValue } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Create from "./pages/Create"
+import Navbar from "./components/Navbar"
 
 
 function App() {
 
   return (
-    <>
-      <Button>Click</Button>
-    </>
+    <Box minH={"100vh"}  bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/create" element={<Create/>} />
+
+      </Routes>
+    </Box>
   )
 }
 
